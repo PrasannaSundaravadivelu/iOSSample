a69634fe6b21b717a1bc7f9dd0798a67f1bc8b3f
prasanna <prasanna.sundaravadivelu@rntbci.com>
2017-06-28 15:46:02 +0530

    <Message>. Related to #<branch no> [Jira number]

--- prasannaExample/ViewController.swift
+++ prasannaExample/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        //test
+        //test12
         // Dispose of any resources that can be recreated.
     }
 
