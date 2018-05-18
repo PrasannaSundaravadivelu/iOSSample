956a3673f21824c393e66f7ef669863be66d089a
Prasanna Sundaravadivelu <prasanna.sundaravadivelu@rntbci.com>
2018-05-18 16:44:52 +0530

    commit

--- prasannaExample/ViewController.swift
+++ prasannaExample/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         //test7634534
-        merge conflicts summa change
+        //merge conflicts summa change
         // Dispose of any resources that can be recreated.
     }
 
